d760df2582d5d2a82818ea1172f4501eb579bb6e
Kate-Dt <dotsiuk.k@gmail.com>
2019-03-25 01:33:47 +0200

    added classes

---
+++ src/main/com/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package main.com.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
